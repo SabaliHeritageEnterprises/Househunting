beab0f274125a1b06e068afd0d3c8da0bd5ac663
bigman-bazu <sabaliheritage@gmail.com>
2026-08-15 10:21:06 -0800

    Fix case-sensitive imports in models/index.js for Vercel

--- models/index.js
+++ models/index.js
@@ -1,9 +1,9 @@
 // models/index.js
-const User = require('./User');
-const Property = require('./Property');
-const Booking = require('./Booking');
-const Report = require('./Report');
-const HouseHunt = require('./HouseHunt');
+const User = require('./user');
+const Property = require('./property');
+const Booking = require('./booking');
+const Report = require('./report');
+const HouseHunt = require('./househunt');
 
 module.exports = {
   User,
